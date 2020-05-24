7551dc2fddf354972f6a8e4e18603d94ef224f4b
EL FAIZ MOHAMED <melfaiz@enseirb-matmeca.fr>
2020-05-24 17:06:38 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,13 @@
 <?php 
 
 
-$dbhost = $_SERVER['RDS_HOSTNAME'];
-$dbport = $_SERVER['RDS_PORT'];
-$dbname = $_SERVER['RDS_DB_NAME'];
+$dbhost = 'localhost';
+$dbname = 'hec_exam_schedule';
 $charset = 'utf8' ;
 
-$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
-$username = $_SERVER['RDS_USERNAME'];
-$password = $_SERVER['RDS_PASSWORD'];
+$dsn = "mysql:host={$dbhost};dbname={$dbname};charset={$charset}";
+$username = 'root';
+$password = '';
 
 $bdd = new PDO($dsn, $username, $password,array('PDO::ATTR_PERSISTENT' => confitem('database', 'pconnect')));
 
